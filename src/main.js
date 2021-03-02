ce1b78376034146be203f95240cb4543a9eebf52
”iapetus2” <khafizov.iskander.lg.g2@gmail.com>
2021-03-02 13:32:38 +0300

    patch 0.3

--- src/main.js
+++ src/main.js
@@ -5,15 +5,6 @@ import router from "@/router";
 import VueRouter from 'vue-router';
 import store from './store';
 import VeeValidate from 'vee-validate';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import {
-  faHome,
-  faUser,
-  faUserPlus,
-  faSignInAlt,
-  faSignOutAlt
-} from '@fortawesome/free-solid-svg-icons';
 
 Vue.config.productionTip = false;
 
